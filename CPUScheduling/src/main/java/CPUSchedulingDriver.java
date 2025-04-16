7b4cd2bf51610f683b64c5571724b018c5b64102
Christian-Mirt <cmirt001@odu.edu>
2025-04-15 21:20:30 -0400

    Initialized repository

---
+++ CPUScheduling/src/main/java/CPUSchedulingDriver.java
@@ -0,0 +1,10 @@
+public class CPUSchedulingDriver {
+    /**
+     * The main function used to demonstrate the TemperatureParser class.
+     *
+     * @param args used to pass in a single filename
+     */
+    public static void main(String[] args) {
+        System.out.println("Program works!");
+    }
+}
---
+++ ProcessSynchronization/src/main/java/ProcessSynchronizationDriver.java
@@ -0,0 +1,10 @@
+public class ProcessSynchronizationDriver {
+    /**
+     * The main function used to demonstrate the TemperatureParser class.
+     *
+     * @param args used to pass in a single filename
+     */
+    public static void main(String[] args) {
+        System.out.println("Program works!");
+    }
+}
